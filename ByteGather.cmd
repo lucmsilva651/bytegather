8dc24a481479819f10bc083714878d1c67a128ca
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-08-23 15:54:34 -0300

    String changes + All new ByteShell

--- ByteGather.cmd
+++ ByteGather.cmd
@@ -3,7 +3,7 @@
 title ByteGather
 
 if "%1"=="" (
-    echo [ByteGather] Use: %0 ^<URL^>
+    echo Use: %0 ^<URL^>
     exit /b 1
 )
 
@@ -14,10 +14,10 @@ certutil -urlcache -split -f "%url%" "%filename%" > nul
 
 if %errorlevel% equ 0 (
     echo.
-    echo [ByteGather] Download complete: %filename%
-    echo [ByteGather] Press any key to exit.
+    echo Download complete: "%filename%"
+    echo Press any key to exit.
     pause >nul
 ) else (
-    echo [ByteGather] Download failed by an unknown reason. Press any key to exit.
+    echo Download failed by an unknown reason. Press any key to exit.
     pause >nul
 )
--- ByteShell.cmd
+++ ByteShell.cmd
@@ -1,6 +1,44 @@
 @echo off
+setlocal
+
+:mainMenu
+cls
 title ByteGather
-echo ByteGather [Version 1.0.0.0]
+echo ByteGather [Version 1.1.0.0]
 echo (c) Lucas Gabriel (lucmsilva). All rights reserved.
 echo.
-cmd /k
+
+set "link="
+set /p "link=Enter the link to the file: "
+
+if "%link%"=="" (
+    echo No link provided.
+    pause
+    goto mainMenu
+)
+
+echo Link provided: %link%
+echo.
+
+set "confirmation="
+if "%~1"=="--force" (
+    set "confirmation=Y"
+) else (
+    set /p "confirmation=Confirm download? (Y/N): "
+)
+
+if /i "%confirmation%"=="Y" (
+    call :downloadFile "%link%"
+) else (
+    echo Download canceled.
+    pause
+    goto mainMenu
+)
+
+pause
+exit
+
+:downloadFile
+echo Calling ByteGather to download: %1
+call ByteGather.cmd %1
+exit
